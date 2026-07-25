b680bc980ecfc136dd7f6a303864fac3c5138026
ST10504208 <st10504208@myemeris.edu.za>
2026-07-25 18:26:57 +0200

    Stored the words individually in their own index of text

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,88 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public ArrayList<String> text = new ArrayList<>();
     ArrayList<Character> alphabet = new ArrayList<>();
+    ArrayList<String> encryptedText = new ArrayList<>();
 
-    static void main(String[] args) {
+    static void main(String[] args){
         Main main = new Main();
-
         main.populateArray();
         main.readFromFile();
-        main.encrypt();
+      //  main.encrypt();
 
     }
 
     void populateArray() {
-        for (int i = 0; i < 26; i++) {
-            alphabet.add((char) ('a' + i));                                                 //Loops through the array of the alphabet and adds the letters to each
+        for (int i = 0; i < 25; i++) {
+            alphabet.add((char) ('a' + i));                                                                //Loops through the array of the alphabet and adds the letters to each
         }
     }
 
     void readFromFile() {
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader("Text.txt"));                                       //Reads the text into a buffered reader (essentially line by line)
-            String line;
-            while ((line = reader.readLine()) != null) {                                                   //Makes sure that it's looped through the entire file
-                text.add(line);                                                                            //Adds the new line to the string and then passes to the encryption metho
+        try(Scanner scFile = new Scanner(new FileInputStream("Text.txt"))){
+            while (scFile.hasNext()) {
+                text.add(scFile.next());
             }
-            System.out.println(text);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
+        catch(IOException e){
+            System.out.println("Error with file: "+e.getMessage());
+        }
     }
+
+
     void encrypt(){
-        //Loop though every char of the string of each index of the array to get each char/letter
-        //Then shift the letters using the letters[] array from earlier
-        char Letter;
-        String word ="";
-        int indexLetter;
         for (int i =0; i<text.size();i++){
-           word = text.get(i);
-           for (int l=0; l<word.length();l++){
+            char Letter;
+            String wordFromText;
+            int indexLetter;
+            String encryptedWord= "";
+            StringBuilder stringBuilder = new StringBuilder();
+            wordFromText = text.get(i);
+            String wholeWord="";
 
-               Letter = word.charAt(l);
-               System.out.println("letter: "+Letter);
+           for (int l=0; l<wordFromText.length();l++) {
+               Letter = wordFromText.charAt(l);
 
-               indexLetter = alphabet.indexOf(Letter);
-               System.out.println("indexLetter: "+indexLetter);
+               if (!Character.isLetterOrDigit(Letter)){
+                   //Somehow separate each word into its own variable to encrypt the letters
+                   System.out.println("special character: "+Letter +" at index: "+l);
+                   if (Character.isWhitespace(Letter)){
+                       int j=l;
+                       char blankLetter;
 
-               Letter = alphabet.get(indexLetter+2);
+                       while (true){
+                           blankLetter = wordFromText.charAt(j-1);
+                           if (Character.isWhitespace(blankLetter)){
+                               wholeWord = wordFromText.substring(j,i+1);
+                               break;
+                           } else{
+                               j--;
+                           }
+                       }
+                   }
+               }
+               for (int y=0; y< wholeWord.length();y++){
+                   Letter = wholeWord.charAt(y);
+                   indexLetter = alphabet.indexOf(Letter);
+                   Letter = alphabet.get(indexOverZ(indexLetter));
+                   encryptedWord = String.valueOf(stringBuilder.append(Letter));
+               }
 
-                //text.set(i,l);
-               System.out.println("new letter: "+Letter);
-
-            }
+           }
+            //System.out.println("encryptedWord: "+encryptedWord);
+            encryptedText.add(encryptedWord);
         }
+        System.out.println(encryptedText);
+    }
 
+    int indexOverZ(int indexLetter){
+        if (indexLetter >=24){
+            indexLetter =25-indexLetter-1;
+        }
+        return indexLetter+2;
     }
 }
 
